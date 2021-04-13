d85f03ca70e9e054373a433aefd5b963d38c6082
Mentra20 <yann.martindes@orange.fr>
2021-04-13 16:31:36 +0200

    Draw Card Buttons + Grid

---
+++ Team-Party-Drinking-Game/Assets/scripts/DrawCards.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+    public GameObject Card;
+    public GameObject PlayerZone;
+    public GameObject EnemyZone;
+
+    void Start()
+    {
+        Debug.Log("Started");
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("Clicked");
+        for(int i =0; i<5; i++)
+        {
+            GameObject playerCard = Instantiate(Card, new Vector3(0, 0, 0), Quaternion.identity);
+            playerCard.transform.SetParent(PlayerZone.transform, false);
+        }
+    }
+}
