c3556a1811420c20663a0bd96a3af266527e3a66
Mentra20 <yann.martindes@orange.fr>
2021-04-13 21:29:59 +0200

    Prevent drag card on table

--- Team-Party-Drinking-Game/Assets/scripts/DrawCards.cs
+++ Team-Party-Drinking-Game/Assets/scripts/DrawCards.cs
@@ -7,6 +7,7 @@ public class DrawCards : MonoBehaviour
 {
     public GameObject Card;
     public GameObject Zone;
+    public int NumberOfCardDrawned;
 
     void Start()
     {
@@ -16,7 +17,7 @@ public class DrawCards : MonoBehaviour
     public void OnClick()
     {
         Debug.Log("Clicked");
-        for(int i = 0; i < 1; i++)
+        for(int i = 0; i < NumberOfCardDrawned; i++)
         {
             GameObject playerCard = Instantiate(Card, new Vector3(0, 0, 0), Quaternion.identity);
             playerCard.transform.SetParent(Zone.transform, false);
